f7ce9b5e0363f47886bba07a7ab58258c384c3b8
Daniel Aranda <c-daniel.aranda@charter.com>
2024-08-27 08:40:18 -0600

    Seccion 13: Custom hooks part 2

---
+++ 06-custom-hooks/useTodo/todoReducer.js
@@ -0,0 +1,16 @@
+export const todoReducer = (state, action) => {
+  switch (action.type) {
+    case "[TODO] Add Todo":
+      return [...state, action.payload];
+    //throw new Error('The action is not implemented');
+    case "[TODO] Delete Todo":
+      return state.filter((todo) => todo.id !== action.payload);
+    case "[TODO] Toggle Todo":
+      return state.map((todo) => ({
+        ...todo,
+        done: todo.id === action.payload ? !todo.done : todo.done,
+      }));
+    default:
+      return state;
+  }
+};
--- 06-custom-hooks/useTodo/useTodo.js
+++ 06-custom-hooks/useTodo/useTodo.js
@@ -1,32 +1,32 @@
-import { useEffect, useReducer } from 'react';
-import { todoReducer } from '../08-useReducer/todoReducer';
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "./todoReducer";
 
 // Get todos from localstorage
-const init = () => JSON.parse(localStorage.getItem('todos')) || [];
+const init = () => JSON.parse(localStorage.getItem("todos")) || [];
 
 export const useTodo = () => {
   const [todos, dispatch] = useReducer(todoReducer, [], init);
 
   // Look into todos changes and then save new todos list in localstorage
   useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos));
+    localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
 
   // Add new todo into the state
   const handleNewTodo = (todo) => {
-    const action = { type: '[TODO] Add Todo', payload: todo };
+    const action = { type: "[TODO] Add Todo", payload: todo };
     dispatch(action);
   };
 
   // Delete todo from state by id
   const handleDeleteTodo = (id) => {
-    const action = { type: '[TODO] Delete Todo', payload: id };
+    const action = { type: "[TODO] Delete Todo", payload: id };
     dispatch(action);
   };
 
   // Toggle a todo from state by id
   const handleToggleTodo = (id) => {
-    const action = { type: '[TODO] Toggle Todo', payload: id };
+    const action = { type: "[TODO] Toggle Todo", payload: id };
     dispatch(action);
   };
 
